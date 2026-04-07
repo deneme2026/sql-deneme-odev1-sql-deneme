e6bf2eace25867bf52ea19f1778989691ae5e5a9
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:24:44 +0000

    12.sql cözüm

--- 12.sql
+++ 12.sql
@@ -1,2 +1,7 @@
 -- 12.sql'de, hem Johnny Depp hem de Helena Bonham Carter'ın birlikte rol aldığı tüm filmlerin isimlerini (title) listeleyen bir SQL sorgusu yazın.
 -- Sorgunuz, her filmin ismi için tek bir sütun içeren bir tablo döndürmelidir.
+
+SELECT title FROM movies 
+JOIN stars s1 ON movies.id = s1.movie_id JOIN people p1 ON s1.person_id = p1.id
+JOIN stars s2 ON movies.id = s2.movie_id JOIN people p2 ON s2.person_id = p2.id
+WHERE p1.name = 'Johnny Depp' AND p2.name = 'Helena Bonham Carter';
