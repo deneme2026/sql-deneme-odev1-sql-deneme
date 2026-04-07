e5be01f32ce802cc43723d7f396aa8a6d738e3d6
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:22:04 +0000

    8.sql cözüm

--- 8.sql
+++ 8.sql
@@ -1,3 +1,8 @@
 -- 8.sql'de, "Toy Story" (Oyuncak Hikayesi) adlı filmde rol almış tüm kişilerin isimlerini (name) listeleyen bir SQL sorgusu yazın.
 -- Sorgunuz, her kişinin adı için tek bir sütun içeren bir tablo döndürmelidir.
 -- Veritabanında "Toy Story" isminde sadece tek bir film olduğunu varsayabilirsiniz.
+
+SELECT name FROM people 
+JOIN stars ON people.id = stars.person_id 
+JOIN movies ON stars.movie_id = movies.id 
+WHERE movies.title = 'Toy Story';
