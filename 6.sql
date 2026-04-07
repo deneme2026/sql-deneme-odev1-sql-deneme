84c81e0a6a944fa026368316600990d5b0578be6
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:20:44 +0000

    6.sql cözüm

--- 6.sql
+++ 6.sql
@@ -1,2 +1,6 @@
 -- 6.sql'de, 2012 yılında vizyona giren tüm filmlerin ortalama reytingini (rating) hesaplayan bir SQL sorgusu yazın.
 -- Sorgunuz, tek bir sütun ve tek bir satır (sadece ortalama puan) içeren bir tablo döndürmelidir.
+
+SELECT AVG(rating) FROM ratings 
+JOIN movies ON ratings.movie_id = movies.id 
+WHERE movies.year = 2012;
