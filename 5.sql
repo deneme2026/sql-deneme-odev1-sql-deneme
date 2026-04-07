af2d3121485653821b2b838b7ac555a97d0de5cf
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:20:19 +0000

    5.sql cözüm

--- 5.sql
+++ 5.sql
@@ -1,3 +1,5 @@
 -- 5.sql'de, tüm "Harry Potter" filmlerinin isimlerini (title) ve vizyon yıllarını (year) kronolojik sıraya (yıllara göre) listeleyen bir SQL sorgusu yazın.
 -- Çıktıda birinci sütun filmin ismi, ikinci sütun ise yılı olmalıdır.
 -- Her filmin isminin "Harry Potter" ile başladığını varsayabilirsiniz.
+
+SELECT title, year FROM movies WHERE title LIKE 'Harry Potter and%' ORDER BY year ASC;
