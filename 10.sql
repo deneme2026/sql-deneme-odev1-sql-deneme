13c0bbd0039c0cd5ba258a351aa86afe97bb34be
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:23:30 +0000

    10.sql cözüldü

--- 10.sql
+++ 10.sql
@@ -1,3 +1,8 @@
 -- 10.sql'de, en az 9.0 puan almış herhangi bir filmi yönetmiş tüm kişilerin isimlerini (name) listeleyen bir SQL sorgusu yazın.
 -- Sorgunuz, her yönetmenin adı için tek bir sütun içeren bir tablo döndürmelidir.
 -- Aynı kişi birden fazla yüksek puanlı film yönettiyse bile isminin listede bir kez (tekrarsız) yer almasını sağlayın.
+
+SELECT DISTINCT name FROM people 
+JOIN directors ON people.id = directors.person_id 
+JOIN ratings ON directors.movie_id = ratings.movie_id 
+WHERE ratings.rating >= 9.0;
