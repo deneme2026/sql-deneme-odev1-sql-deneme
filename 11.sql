25b845003d514bc91d94eb89a515b394633b38de
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:24:10 +0000

    11.sql cözüm

--- 11.sql
+++ 11.sql
@@ -1,3 +1,10 @@
 -- 11.sql'de, Chadwick Boseman'ın rol aldığı filmler arasından en yüksek puana sahip (en iyi) 5 filmin ismini (title) listeleyen bir SQL sorgusu yazın.
 -- Liste en yüksek puanlı filmden başlayarak azalan sırayla dizilmelidir.
 -- Çıktı sadece film isimlerini barındıran tek bir sütundan oluşmalıdır.
+SELECT title FROM movies 
+JOIN stars ON movies.id = stars.movie_id 
+JOIN people ON stars.person_id = people.id 
+JOIN ratings ON movies.id = ratings.movie_id 
+WHERE people.name = 'Chadwick Boseman' 
+ORDER BY ratings.rating DESC 
+LIMIT 5;
