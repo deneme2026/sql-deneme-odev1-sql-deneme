fa9471c5cec7a4f14bfb406c3bb121b02edd3641
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:19:13 +0000

    3.sql cözüm

--- 3.sql
+++ 3.sql
@@ -1,2 +1,4 @@
 -- 3.sql'de, 2018 veya sonrasında vizyona giren tüm filmlerin isimlerini (title) alfabetik sırayla listeleyen bir SQL sorgusu yazın.
 -- Sorgunuz, her film için tek bir sütun içeren bir tablo döndürmelidir.
+
+SELECT title FROM movies WHERE year >= 2018 ORDER BY title ASC;
