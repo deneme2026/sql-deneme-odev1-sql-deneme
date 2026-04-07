604b2dbcf987368d91e94abc3b53d3df60685d74
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:21:27 +0000

    7.sql cözüldü

--- 7.sql
+++ 7.sql
@@ -1,3 +1,8 @@
 -- 7.sql'de, 2010 yılında vizyona giren tüm filmleri ve reytinglerini listeleyen, en yüksek puandan en düşük puana (azalan sırayla) dizilmiş bir SQL sorgusu yazın.
 -- Aynı reytinge sahip filmler varsa, onları kendi içlerinde başlıklarına (title) göre alfabetik olarak sıralayın.
 -- Çıktıda iki sütun bulunmalıdır: Filmin ismi (title) ve reytingi (rating).
+
+SELECT movies.title, ratings.rating FROM movies 
+JOIN ratings ON movies.id = ratings.movie_id 
+WHERE movies.year = 2010 
+ORDER BY ratings.rating DESC, movies.title ASC;
