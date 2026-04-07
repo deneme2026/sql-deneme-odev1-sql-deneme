7dc66e1ad9e1f27fb66408dc5a150b3a74a3f66b
Talip BOZKURT <146137008+talipbozkurt@users.noreply.github.com>
2026-04-07 20:25:24 +0000

    13.sql cözüm

--- 13.sql
+++ 13.sql
@@ -2,3 +2,12 @@
 -- Sorgunuz, her kişinin adı için tek bir sütun içeren bir tablo döndürmelidir.
 -- Veritabanında "Kevin Bacon" adında birden fazla kişi olabilir. Sadece 1958 doğumlu Kevin Bacon'ı seçtiğinizden emin olun.
 -- Sonuç listesinde Kevin Bacon'ın kendisi DAHİL EDİLMEMELİDİR.
+
+SELECT DISTINCT name FROM people 
+JOIN stars ON people.id = stars.person_id 
+WHERE stars.movie_id IN (
+    SELECT movie_id FROM stars 
+    JOIN people ON stars.person_id = people.id 
+    WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
+) 
+AND people.name != 'Kevin Bacon';
